63bd77ae0cc0085efd65f099843354f54325c094
vladyslav_saliuk <vlidik1345@gmail.com>
2023-02-10 18:19:41 +0200

    add new lib env

--- source/screens/App/Dashboard/Dashboard.tsx
+++ source/screens/App/Dashboard/Dashboard.tsx
@@ -4,12 +4,15 @@ import React, { memo, useCallback } from 'react';
 import { Text, View, Button, I18nManager } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import RNRestart from 'react-native-restart';
+import Config from 'react-native-config';
 // hooks
 import { useTranslation } from '~/hooks';
 
 export const Dashboard: React.FC<any> = memo(() => {
   const insets = useSafeAreaInsets();
   const { t, i18n } = useTranslation();
+
+  console.log(Config.API_DEV_URL);
 
   const handlePressChangeLanguage = useCallback(() => {
     i18n.changeLanguage(i18n.language === 'ar' ? 'en' : 'ar').then(() => {
